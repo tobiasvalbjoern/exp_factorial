d2de0ba4e1193fa747de6b40e2df98fef1d13e44
Tobias Valbjørn <tvalbjoern@gmail.com>
2017-09-26 14:00:25 +0200

    Template sum_n up and running

---
+++ Debug/src/exp_factorial.d
@@ -0,0 +1,3 @@
+src/exp_factorial.o: ../src/exp_factorial.cpp ../src/sum_n.h
+
+../src/sum_n.h:
---
+++ src/sum_n.h
@@ -0,0 +1,13 @@
+#ifndef SUM_N_INCLUDED
+#define SUM_N_INCLUDED
+
+using namespace std;
+
+template<class T>
+T sum_n(T x)
+{
+	cout << x << endl;
+	return  x;
+}
+
+#endif // ADD_H_INCLUDED 
